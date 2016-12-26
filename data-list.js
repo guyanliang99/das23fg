bfb80afa8cb0f2964e14e183d2440b4c1d4ef734
guyanliang99 <86375034@qq.com>
2016-12-26 16:04:41 +0800

    1`
    2

---
+++ data-list.js
@@ -0,0 +1,199 @@
+window.dataList = [
+	{
+            content: '凌晨三点半的视频监控录像，谁看谁傻眼',
+            id: '40',
+            title: '凌晨三点半的视频监控录像，谁看谁傻眼',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=y1414o3epj1&tiny=0&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/40.jpg'
+        },
+		{
+            content: '村民目睹水鬼拉人，全村人把水库抽干，结果看到很恐怖的一幕.....',
+            id: '0',
+            title: '村民目睹水鬼拉人，全村人把水库抽干，结果看到很恐怖的一幕.....',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=h0312zwd00i&tiny=0&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/4.jpg'
+        },
+        {
+            content: '工地施工，巨蟒出山，当场咬死数人场面血……腥',
+            id: '19',
+            title: '工地施工，巨蟒出山，当场咬死数人场面血……腥',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=l03071qs8nl&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/18.jpg'
+        },
+        {
+            content: '黒社會开个宝马装逼，遇到退伍军人，场面火瀑',
+            id: '27',
+            title: '黒社會开个宝马装逼，遇到退伍军人，场面火瀑',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=p0305ee12z9&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/26.png'
+        },
+        {
+            content: '巨蟒渡劫，附件村子全部被毁！太恐怖了',
+            id: '4',
+            title: '巨蟒渡劫，附件村子全部被毁！太恐怖了',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=d0311ifliv3&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/3.jpg'
+        },
+        {
+            content: '碰瓷遇到黑社会老大当场被打死',
+            id: '21',
+            title: '碰瓷遇到黑社会老大当场被打死',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=v03320s50ov&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/26.jpg'
+        },
+        {
+            content: '小伙回家遭遇恐怖的灵异事件 被无腿女鬼跟踪',
+            id: '37',
+            title: '小伙回家遭遇恐怖的灵异事件 被无腿女鬼跟踪',
+            videoSrc: 'http://v.qq.com/iframe/player.html?vid=g0319t2zbz9&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/37.jpg'
+        },
+        {
+            content: '离奇！女子给丈夫上坟听到坟里有声音 挖出棺材发现丈夫还在动',
+            id: '33',
+            title: '离奇！女子给丈夫上坟听到坟里有声音 挖出棺材发现丈夫还在动',
+            videoSrc: 'http://v.qq.com/iframe/player.html?vid=k0320h51zt6&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/33.jpg'
+        },
+        {
+            content: '女孩半夜喝醉遭人“捡尸”， 衣服被扒光拍下裸照',
+            id: '3',
+            title: '女孩半夜喝醉遭人“捡尸”， 衣服被扒光拍下裸照',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=q0315k1n5sl&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/2.jpg'
+        },
+        {
+            content: '孩子做灵车上学，怪事暗藏惊人秘密！！！',
+            id: '32',
+            title: '孩子做灵车上学，怪事暗藏惊人秘密！！！',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=j0321r60zct&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/32.jpg'
+        },
+        {
+            content: '【实拍】农民工大战黒社會，直接开铲车撞',
+            id: '6',
+            title: '【实拍】农民工大战黒社會，直接开铲车撞',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=v0146u3eyv7&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/5.jpg'
+        },
+        {
+            content: '村干部不雅视蘋曝光：吃着饭掀起衣服摸摸摸！',
+            id: '8',
+            title: '村干部不雅视蘋曝光：吃着饭掀起衣服摸摸摸！',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=f0018vtrkog&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/7.jpg'
+        },
+        {
+            content: '记者冒死 暗访多名外籍黑人女子，她们尽然做这个.......',
+            id: '9',
+            title: '记者冒死 暗访多名外籍黑人女子，她们尽然做这个.........',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=k0307av7gx8&tiny=0&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/8.jpg'
+        },
+        {
+            content: '实拍女老师竟然对男学生当场做这个......太那个了，看不下去了',
+            id: '10',
+            title: '实拍女老师竟然对男学生当场做这个......太那个了，看不下去了',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=y0307mrheq9&tiny=0&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/9.jpg'
+        },
+        {
+            content: '女生上厕所时被跟踪！没想到监视器竟然录下这可怕的一幕',
+            id: '11',
+            title: '女生上厕所时被跟踪！没想到监视器竟然录下这可怕的一幕',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=u0313qjb7wn&tiny=0&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/10.jpg'
+        },
+        {
+            content: '实拍老汉超市猥琐女顾客他竟然对女顾客的臀部做这些事',
+            id: '13',
+            title: '实拍老汉超市猥琐女顾客他竟然对女顾客的臀部做这些事',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=y0308hwte59&tiny=0&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/12.jpg'
+        },
+        {
+            content: '原配抓小三，当街撕光衣服，一天删……',
+            id: '14',
+            title: '原配抓小三，当街撕光衣服，一天删……',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=e0311xmjtq4&tiny=0&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/13.jpg'
+        },
+        {
+            content: '让世人愤怒的闹洞房，史上最无耻没有之一',
+            id: '16',
+            title: '让世人愤怒的闹洞房，史上最无耻没有之一',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=a03042cc31v&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/15.jpg'
+        },
+        {
+            content: '女的太风骚了吧，竟然直接在车上和男友干这事！！！！',
+            id: '17',
+            title: '女的太风骚了吧，竟然直接在车上和男友干这事！！！！',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=c01927vvhr4&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/16.jpg'
+        },
+        {
+            content: '母.猪体内发现2岁孩子，吓人！',
+            id: '18',
+            title: '母.猪体内发现2岁孩子，吓人！',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=f015418dxbm&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/17.jpg'
+        },
+        {
+            content: '恐怖！监视器下拍到女子被鬼影跟踪太吓人了',
+            id: '23',
+            title: '恐怖！监视器下拍到女子被鬼影跟踪太吓人了',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=t03203eqsg5&tiny=0&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/22.jpg'
+        },
+        {
+            content: '火了！！一个退伍老兵单挑10个城管！全部撂倒',
+            id: '24',
+            title: '火了！！一个退伍老兵单挑10个城管！全部撂倒',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=w0189um0875&tiny=0&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/23.jpg'
+        },
+        {
+            content: '太恐怖了！转身瞬间被掐头！女主播直播中“撞鬼”',
+            id: '26',
+            title: '太恐怖了！转身瞬间被掐头！女主播直播中“撞鬼”',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=l0305ywnb3e&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/25.jpg'
+        },
+        {
+            content: '泰国gay版『非诚勿扰』，污爆了，这尺度奔放得我三观尽毁……',
+            id: '31',
+            title: '泰国gay版『非诚勿扰』，污爆了，这尺度奔放得我三观尽毁……',
+            videoSrc: 'https://v.qq.com/iframe/player.html?vid=l0319iuqdq7&auto=0',
+            imageUrl: 'http://celi-image.oss-cn-beijing.aliyuncs.com/30.jpg'
+        }
+];
+
+window.adList = [
+
+];
+window.inPageAds = [
+
+];
+
+window.adScripts = [
+
+];
+
+window.backAds = [{
+		//isFixed: false,
+		//path: "http://short.weixinmongo.com/s_522032789848065"
+	}];
+
+
+window.apiList = [
+    "//dc.spiderer.cn/api/DomainModel/NextDomainB"
+];
+
+window.navData = {
+    nickName: '猎奇王子',
+    url: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzI3MzU4MTQzOQ==#wechat_redirect',
+	complainFile:'/dc.html?url='
+}
+
+window.forceShareCount = 3;
